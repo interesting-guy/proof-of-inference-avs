798e24217d9c7ed47b098b17c3bca395e7944511
interesting-guy <117534087+interesting-guy@users.noreply.github.com>
2025-07-08 21:28:03 +0530

    Update ProofOfInferenceAVS.js

--- test/ProofOfInferenceAVS.js
+++ test/ProofOfInferenceAVS.js
@@ -1,49 +1,37 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-// Use Ethers v6+ parseEther import
-const { parseEther } = require("ethers/lib/utils");
-
 describe("ProofOfInferenceAVS", function () {
   let avs;
-  let owner;
-  let submitter;
-  let operator;
+  let owner, submitter, operator1, operator2;
+  const model = "test-model";
+  const inputHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("some prompt"));
 
-  const modelId = "test-model";
-  const inputHash = ethers.keccak256(ethers.toUtf8Bytes("test prompt"));
-  const outputHash = ethers.keccak256(ethers.toUtf8Bytes("test output"));
-
-  beforeEach(async () => {
-    [owner, submitter, operator] = await ethers.getSigners();
-
+  beforeEach(async function () {
+    [owner, submitter, operator1, operator2] = await ethers.getSigners();
     const AVS = await ethers.getContractFactory("ProofOfInferenceAVS");
-    avs = await AVS.deploy(modelId);
+    avs = await AVS.deploy();
     await avs.deployed();
   });
 
   it("should register an operator", async function () {
-    await avs.connect(owner).registerOperator(operator.address, "node-1");
-    const isRegistered = await avs.operators(operator.address);
-    expect(isRegistered.registered).to.be.true;
+    await avs.connect(owner).registerOperator(operator1.address);
+    const isRegistered = await avs.operators(operator1.address);
+    expect(isRegistered).to.be.true;
   });
 
-  it("should submit an inference correctly", async function () {
-    await avs.connect(owner).registerOperator(operator.address, "node-1");
+  it("should submit and verify a proof", async function () {
+    await avs.connect(owner).registerOperator(operator1.address);
 
-    await avs
-      .connect(submitter)
-      .submitInference(
-        modelId,
-        inputHash,
-        outputHash,
-        operator.address,
-        parseEther("1.0")
-      );
+    const resultHash = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes("result hash")
+    );
+    const signature = ethers.utils.hexlify(ethers.utils.randomBytes(65));
 
-    const inf = await avs.inferences(0);
-    expect(inf.submitter).to.equal(submitter.address);
-    expect(inf.operator).to.equal(operator.address);
-    expect(inf.modelId).to.equal(modelId);
+    await avs.connect(operator1).submitProof(model, inputHash, resultHash, signature);
+    const storedProof = await avs.proofs(model, inputHash);
+
+    expect(storedProof.resultHash).to.equal(resultHash);
+    expect(storedProof.submitter).to.equal(operator1.address);
   });
 });
