fddff5120d971d76629083d1b0031ff57a2c6cf5
interesting-guy <117534087+interesting-guy@users.noreply.github.com>
2025-07-08 21:23:56 +0530

    Update ProofOfInferenceAVS.js

--- test/ProofOfInferenceAVS.js
+++ test/ProofOfInferenceAVS.js
@@ -1,80 +1,49 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+// Use Ethers v6+ parseEther import
+const { parseEther } = require("ethers/lib/utils");
+
 describe("ProofOfInferenceAVS", function () {
-  let avs, owner, submitter, operators;
-  const model = "test-model";
-  const inputHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("some prompt"));
-  const resultHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("result"));
+  let avs;
+  let owner;
+  let submitter;
+  let operator;
+
+  const modelId = "test-model";
+  const inputHash = ethers.keccak256(ethers.toUtf8Bytes("test prompt"));
+  const outputHash = ethers.keccak256(ethers.toUtf8Bytes("test output"));
 
   beforeEach(async () => {
-    [owner, submitter, ...operators] = await ethers.getSigners();
+    [owner, submitter, operator] = await ethers.getSigners();
+
     const AVS = await ethers.getContractFactory("ProofOfInferenceAVS");
-    avs = await AVS.deploy();
+    avs = await AVS.deploy(modelId);
     await avs.deployed();
   });
 
-  async function registerOperators(count) {
-    for (let i = 0; i < count; i++) {
-      await avs.connect(operators[i]).registerOperator({ value: ethers.utils.parseEther("1") });
-    }
-  }
-
-  async function submitTask() {
-    const tx = await avs.connect(submitter).submitInferenceTask(model, inputHash);
-    const rc = await tx.wait();
-    const event = rc.events.find((e) => e.event === "InferenceSubmitted");
-    return event.args.taskId;
-  }
-
-  it("Should go through full lifecycle: register, submit task, reach consensus", async () => {
-    await registerOperators(5);
-    const taskId = await submitTask();
-
-    for (let i = 0; i < 5; i++) {
-      await expect(
-        avs.connect(operators[i]).submitResult(taskId, resultHash)
-      ).to.emit(avs, "ResultSubmitted");
-    }
-
-    const task = await avs.tasks(taskId);
-    expect(task.status).to.equal(1); // Completed
-    expect(task.consensusResult).to.equal(resultHash);
-    expect(task.consensusCount).to.equal(5);
+  it("should register an operator", async function () {
+    await avs.connect(owner).registerOperator(operator.address, "node-1");
+    const isRegistered = await avs.operators(operator.address);
+    expect(isRegistered.registered).to.be.true;
   });
 
-  it("Should finalize with majority consensus even if not all results match", async () => {
-    await registerOperators(5);
-    const taskId = await submitTask();
+  it("should submit an inference correctly", async function () {
+    await avs.connect(owner).registerOperator(operator.address, "node-1");
 
-    const hashA = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("A"));
-    const hashB = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("B"));
+    await avs
+      .connect(submitter)
+      .submitInference(
+        modelId,
+        inputHash,
+        outputHash,
+        operator.address,
+        parseEther("1.0")
+      );
 
-    await avs.connect(operators[0]).submitResult(taskId, hashA);
-    await avs.connect(operators[1]).submitResult(taskId, hashA);
-    await avs.connect(operators[2]).submitResult(taskId, hashA);
-    await avs.connect(operators[3]).submitResult(taskId, hashB);
-
-    await expect(
-      avs.connect(operators[4]).submitResult(taskId, hashB)
-    ).to.emit(avs, "TaskFinalized").withArgs(taskId, hashA, 3);
-
-    const task = await avs.tasks(taskId);
-    expect(task.status).to.equal(1);
-    expect(task.consensusResult).to.equal(hashA);
-    expect(task.consensusCount).to.equal(3);
-  });
-
-  it("Should revert if operator submits result after deadline", async () => {
-    await registerOperators(3);
-    const taskId = await submitTask();
-
-    // Fast forward time beyond 1 day (task deadline)
-    await ethers.provider.send("evm_increaseTime", [2 * 24 * 60 * 60]);
-    await ethers.provider.send("evm_mine");
-
-    await expect(
-      avs.connect(operators[0]).submitResult(taskId, resultHash)
-    ).to.be.revertedWith("Task deadline passed");
+    const inf = await avs.inferences(0);
+    expect(inf.submitter).to.equal(submitter.address);
+    expect(inf.operator).to.equal(operator.address);
+    expect(inf.modelId).to.equal(modelId);
   });
 });
